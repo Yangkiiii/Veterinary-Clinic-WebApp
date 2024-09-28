1b72dd1072993282af3885cb491ac2f04955bee9
Yangkiiii <janxander8@gmail.com>
2024-09-29 00:25:59 +0800

    Put images to media, settings mediaroot, deleted html sa labas,

--- VetClinic/myProject/myProject/settings.py
+++ VetClinic/myProject/myProject/settings.py
@@ -122,8 +122,11 @@ MEDIA_URL = '/Media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'Media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
