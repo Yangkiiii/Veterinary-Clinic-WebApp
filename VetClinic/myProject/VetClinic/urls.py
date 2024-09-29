b77b09bc02579fbcc5790924122ea1f5db101e83
Chenzz00 <168355526+Chenzz00@users.noreply.github.com>
2024-09-29 11:09:27 +0800

    Update urls.py

--- VetClinic/myProject/VetClinic/urls.py
+++ VetClinic/myProject/VetClinic/urls.py
@@ -1,22 +1,20 @@
 from django.urls import path
-
-
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('registration', views.registration, name="registration"),
-    path('forgot', views.forgot, name="forgot"),
-    path('mail', views.mail, name="mail"),
-    path('admin', views.admin, name="admin"),
-    path('adhistory', views.adhistory, name="adhistory"),
-    path('vet', views.vet, name="vet"),
-    path('trans', views.trans, name="trans"),
-    path('form', views.form, name="form"),
-    path('change', views.change, name="change"),
-    path('owner', views.owner, name="owner"),
-    path('ownhistory', views.ownhistory, name="ownhistory"),
-    path('appwindow', views.appwindow, name="appwindow"),
-    path('profile', views.profile, name="profile"),
-
+    path('', views.index, name='index'),  # Home page (login)
+    path('registration/', views.registration, name='registration'),  # User registration
+    path('forgot-password/', views.forgot, name='forgot'),  # Password recovery
+    path('Appointment-Schedule/', views.admin, name='admin'),  # Admin login handling
+    path('Client-Records/', views.vet, name='vet'),  # Veterinarian dashboard
+    path('Transaction-History/', views.trans, name='trans'),  # Transaction history
+    path('form/', views.form, name='form'),  # Form submission
+    path('Change-password/', views.change, name='change'),  # Change password
+    path('Homepage/', views.owner, name='owner'),  # Owner details
+    path('profile/', views.profile, name='profile'),  # User profile
+    path('MyHistory/', views.ownhistory, name='ownhistory'),  # Owner's history
+    path('Appointment/', views.appwindow, name='appwindow'),  # Application window
+    path('Admin-History/', views.adhistory, name='adhistory'),  # Admin history
+    path('Account-History/', views.adaccount, name='adaccount'),  # Admin account management
+    path('mail/', views.mail, name= 'mail'),
 ]
